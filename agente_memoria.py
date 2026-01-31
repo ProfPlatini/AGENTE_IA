37b0b328362391bec0d6b16de2bc794c4181e6f9
ProfPlatini <platiniluiz@outlook.com>
2026-01-31 16:53:03 -0300

    Segundo código: I.A. com memória)

---
+++ agente_memoria.py
@@ -0,0 +1,30 @@
+#Meu primeiro agente de IA com a Framework AGNO
+import os
+from dotenv import load_dotenv
+from agno.agent import Agent
+from agno.models.openai import OpenAIChat
+from agno.tools.duckduckgo import DuckDuckGoTools
+
+# Carrega a chave que está no seu arquivo .env
+load_dotenv()
+
+# Criação do Agente autônomo OPENAI
+agente = Agent(
+    model=OpenAIChat(id="gpt-4o-mini"),
+    tools=[DuckDuckGoTools()],
+    description="Você é um assistente de pesquisa prestativo.",
+    instructions=[
+        "Sempre use a ferramenta de busca para confirmar dados atuais.",
+        "Responda em Português do Brasil de forma clara.",
+    ],
+    markdown=True
+)
+
+# Comando para o agente falar
+while True:
+    print("--- AGENTE INICIADO ---")
+    pergunta = input("Digite a sua pergunta: ")
+    if pergunta == "Sair":
+        break
+    else:
+        agente.print_response(pergunta, stream=True)
